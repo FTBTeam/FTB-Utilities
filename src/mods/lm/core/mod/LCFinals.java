0cb2bc7c998783093e1ad5e8d6b557467ad35b30
LatvianModder <latvianmodder@gmail.com>
2014-03-25 20:21:57 +0200

    Fixed Proxy typo

--- src/mods/lm/core/mod/LCFinals.java
+++ src/mods/lm/core/mod/LCFinals.java
@@ -2,9 +2,9 @@ package mods.lm.core.mod;
 
 public class LCFinals
 {
-	public static final String MODID = "lm_latcore";
+	public static final String MODID = "lm.latcore";
 	public static final String MODNAME = "LatCore";
-	public static final String VERSION = "1.0.0.1";
-	public static final String SIDE_CLIENT = "mods.lm_core.mod.LCClient";
-	public static final String SIDE_SERVER = "mods.lm_core.mod.LCCommon";
+	public static final String VERSION = "1.0.1.0";
+	public static final String SIDE_CLIENT = "mods.lm.core.mod.LCClient";
+	public static final String SIDE_SERVER = "mods.lm.core.mod.LCCommon";
 }

26dc41ba0ffd67219529404acb04f3ab51616f6f
LatvianModder <latvianmodder@gmail.com>
2016-10-06 22:28:24 +0300

    FTBLib update

--- src/main/java/com/feed_the_beast/ftbu/config/FTBUConfigLogin.java
+++ src/main/java/com/feed_the_beast/ftbu/config/FTBUConfigLogin.java
@@ -6,7 +6,7 @@ import com.feed_the_beast.ftbl.lib.config.PropertyItemStackList;
 import com.feed_the_beast.ftbl.lib.config.PropertyTextComponentList;
 import com.feed_the_beast.ftbu.FTBUFinals;
 
-import java.util.ArrayList;
+import java.util.Collections;
 
 public class FTBUConfigLogin
 {
@@ -17,8 +17,8 @@ public class FTBUConfigLogin
     public static final PropertyBool ENABLE_STARTING_ITEMS = new PropertyBool(true);
 
     @ConfigValue(id = "login.motd", file = FTBUFinals.MOD_ID, info = "Message of the day. This will be displayed when player joins the server")
-    public static final PropertyTextComponentList MOTD = new PropertyTextComponentList(new ArrayList<>());
+    public static final PropertyTextComponentList MOTD = new PropertyTextComponentList(Collections.emptyList());
 
     @ConfigValue(id = "login.starting_items", file = FTBUFinals.MOD_ID, info = {"Items to give player when he first joins the server", "Format: \"StringID Size Metadata\" or {nbt}"})
-    public static final PropertyItemStackList STARTING_ITEMS = new PropertyItemStackList(new ArrayList<>());
+    public static final PropertyItemStackList STARTING_ITEMS = new PropertyItemStackList(Collections.emptyList());
 }

2e313d9b9281260b674cbf27dfa3a45148990661
LatvianModder <latvianmodder@gmail.com>
2017-04-03 15:59:04 +0300

    Added way to write shorter lang keys with $key in guides, fixed crash with missing items

--- src/main/java/com/feed_the_beast/ftbu/gui/Guides.java
+++ src/main/java/com/feed_the_beast/ftbu/gui/Guides.java
@@ -17,12 +17,14 @@ import com.feed_the_beast.ftbl.lib.info.TexturePageIconRenderer;
 import com.feed_the_beast.ftbl.lib.util.LMJsonUtils;
 import com.feed_the_beast.ftbl.lib.util.LMStringUtils;
 import com.feed_the_beast.ftbl.lib.util.LMUtils;
+import com.feed_the_beast.ftbu.FTBUFinals;
 import com.feed_the_beast.ftbu.api.guide.ClientGuideEvent;
 import com.feed_the_beast.ftbu.api.guide.GuideFormat;
 import com.feed_the_beast.ftbu.api.guide.IGuide;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.IResourceManagerReloadListener;
@@ -66,6 +68,7 @@ public enum Guides implements IResourceManagerReloadListener
     private static boolean isReloading = false;
     private static Thread reloadingThread = null;
     private static GuiInfo cachedGui = null;
+    public static final Map<String, String> SUBSTITUTE_CACHE = new HashMap<>();
 
     public static void setShouldReload()
     {
@@ -85,15 +88,11 @@ public enum Guides implements IResourceManagerReloadListener
                     @Override
                     public void startLoading()
                     {
-                        reloadingThread = new Thread()
+                        reloadingThread = new Thread(() ->
                         {
-                            @Override
-                            public void run()
-                            {
-                                INSTANCE.onResourceManagerReload(Minecraft.getMinecraft().getResourceManager());
-                                isReloading = false;
-                            }
-                        };
+                            INSTANCE.onResourceManagerReload(Minecraft.getMinecraft().getResourceManager());
+                            isReloading = false;
+                        });
                         reloadingThread.start();
                     }
 
@@ -122,11 +121,12 @@ public enum Guides implements IResourceManagerReloadListener
     @Override
     public void onResourceManagerReload(IResourceManager resourceManager)
     {
-        LMUtils.DEV_LOGGER.info("Reloading guides...");
+        FTBUFinals.LOGGER.info("Reloading guides...");
         INFO_PAGE.clear();
         INFO_PAGE.setTitle(new TextComponentString("Guides")); //TODO: Lang
 
         List<IGuide> guides = new ArrayList<>();
+        SUBSTITUTE_CACHE.clear();
 
         for(String domain : resourceManager.getResourceDomains())
         {
@@ -177,6 +177,11 @@ public enum Guides implements IResourceManagerReloadListener
         INFO_PAGE.sort(false);
     }
 
+    private static boolean validChar(char c)
+    {
+        return c == '_' || c == '.' || c == '{' || c == '}' || LMStringUtils.isTextChar(c, true);
+    }
+
     private static void loadTree(IResourceManager resourceManager, String domain, InfoPage page, GuideFormat format, String parentDir) throws Exception
     {
         try
@@ -184,7 +189,7 @@ public enum Guides implements IResourceManagerReloadListener
             switch(format)
             {
                 case JSON:
-                    for(JsonElement e : LMJsonUtils.fromJson(new InputStreamReader(resourceManager.getResource(new ResourceLocation(domain, parentDir + "/index.json")).getInputStream())).getAsJsonArray())
+                    for(JsonElement e : LMJsonUtils.fromJson(replaceSubstitutes(LMStringUtils.readString(resourceManager.getResource(new ResourceLocation(domain, parentDir + "/index.json")).getInputStream()))).getAsJsonArray())
                     {
                         page.println(InfoPageHelper.createLine(page, e));
                     }
@@ -200,9 +205,10 @@ public enum Guides implements IResourceManagerReloadListener
         }
         catch(Exception ex)
         {
-            if(LMUtils.DEV_ENV && !(ex instanceof FileNotFoundException))
+            if(!(ex instanceof FileNotFoundException))
             {
-                ex.printStackTrace();
+                FTBUFinals.LOGGER.error("Failed to load " + page.getFullID() + ": " + ex);
+                //ex.printStackTrace();
             }
         }
 
@@ -253,5 +259,50 @@ public enum Guides implements IResourceManagerReloadListener
                 ex.printStackTrace();
             }
         }
+    }
+
+    private static String replaceSubstitutes(CharSequence text)
+    {
+        StringBuilder builder = new StringBuilder();
+        StringBuilder keyBuilder = new StringBuilder();
+
+        //Dont start with first 2 chars
+        for(int i = 0; i < text.length(); i++)
+        {
+            char c = text.charAt(i);
+
+            if(keyBuilder.length() > 0)
+            {
+                if(validChar(c))
+                {
+                    keyBuilder.append(c);
+                }
+                else
+                {
+                    String value = SUBSTITUTE_CACHE.computeIfAbsent(keyBuilder.substring(1), k -> replaceSubstitutes(I18n.format(k)));
+
+                    //TODO: Add special values
+
+                    builder.append(value);
+
+                    if(c != '$')
+                    {
+                        builder.append(c);
+                    }
+
+                    keyBuilder.setLength(0);
+                }
+            }
+            else if(c == '$' && (i < 1 || text.charAt(i - 1) != '\\'))
+            {
+                keyBuilder.append(c);
+            }
+            else
+            {
+                builder.append(c);
+            }
+        }
+
+        return builder.toString();
     }
 }

5368424f121af81bcee7cccedc30adc005ec18eb
LatvianModder <latvianmodder@gmail.com>
2018-09-15 02:23:20 +0300

    /shrug now supports text

--- src/main/java/com/feed_the_beast/ftbutilities/command/client/CmdShrug.java
+++ src/main/java/com/feed_the_beast/ftbutilities/command/client/CmdShrug.java
@@ -2,6 +2,7 @@ package com.feed_the_beast.ftbutilities.command.client;
 
 import com.feed_the_beast.ftblib.lib.client.ClientUtils;
 import com.feed_the_beast.ftblib.lib.command.CmdBase;
+import com.feed_the_beast.ftblib.lib.util.StringJoiner;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
@@ -19,6 +20,15 @@ public class CmdShrug extends CmdBase
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
 	{
-		ClientUtils.execClientCommand("\u00AF\\_(\u30C4)_/\u00AF");
+		String txt = StringJoiner.with(' ').joinStrings(args);
+
+		if (txt.isEmpty())
+		{
+			ClientUtils.execClientCommand("\u00AF\\_(\u30C4)_/\u00AF");
+		}
+		else
+		{
+			ClientUtils.execClientCommand(txt + " \u00AF\\_(\u30C4)_/\u00AF");
+		}
 	}
 }

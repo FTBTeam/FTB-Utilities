9810c1e6272a4e6512a4215952ccb0ff26359bbe
LatvianModder <latvianmodder@gmail.com>
2018-12-01 02:50:35 +0200

    /scan_items now exports multiple files, soon to be moved to FTB Janitor mod

--- src/main/java/com/feed_the_beast/ftbutilities/command/client/CmdScanItems.java
+++ src/main/java/com/feed_the_beast/ftbutilities/command/client/CmdScanItems.java
@@ -10,15 +10,16 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.FurnaceRecipes;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.oredict.OreDictionary;
 
 import java.io.File;
@@ -61,168 +62,129 @@ public class CmdScanItems extends CmdBase
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, final String[] args) throws CommandException
 	{
-		new Thread("Scanning Items")
-		{
-			@Override
-			public void run()
+		sender.sendMessage(new TextComponentString("Exporting debug files..."));
+
+		new Thread(() -> {
+			List<String> list = new ArrayList<>();
+			for (Fluid fluid : FluidRegistry.getRegisteredFluids().values())
 			{
-				boolean md = args.length > 0 && args[0].equals("md");
+				FluidStack stack = new FluidStack(fluid, 1000);
+				ModContainer mod = Loader.instance().getIndexedModList().get(fluid.getStill(stack).getNamespace());
+				list.add(fluid.getName() + " ;; " + stack.getLocalizedName() + " ;; " + (mod == null ? "Unknown" : mod.getName()));
+			}
+			saveFile(list, "fluids.txt");
+		}).start();
 
-				List<String> list = new ArrayList<>();
-				list.add("# Item format: mod:item metadata {nbt}");
-				list.add("");
-				list.add("## Items with duplicate display names:");
+		new Thread(() -> {
+			List<String> list = new ArrayList<>();
 
-				list.add("");
+			StringBuilder builder = new StringBuilder();
+			NonNullList<ItemStack> stacks = NonNullList.create();
 
-				StringBuilder builder = new StringBuilder();
-				NonNullList<ItemStack> stacks = NonNullList.create();
+			LinkedHashMap<String, LinkedHashSet<ItemEntry>> itemToDisplayNameMap = new LinkedHashMap<>();
 
-				LinkedHashMap<String, LinkedHashSet<ItemEntry>> itemToDisplayNameMap = new LinkedHashMap<>();
-				LinkedHashMap<ItemEntry, Boolean> hasRecipe = new LinkedHashMap<>();
+			for (Item item : Item.REGISTRY)
+			{
+				stacks.clear();
+				item.getSubItems(CreativeTabs.SEARCH, stacks);
 
-				for (Item item : Item.REGISTRY)
+				for (ItemStack stack : stacks)
 				{
-					stacks.clear();
-					item.getSubItems(CreativeTabs.SEARCH, stacks);
+					ItemEntry itemEntry = ItemEntry.get(stack);
 
-					for (ItemStack stack : stacks)
+					if (!stack.isEmpty() && filter(itemEntry))
+					{
+						String displayName = stack.getDisplayName();
+						LinkedHashSet<ItemEntry> set = itemToDisplayNameMap.get(displayName);
+
+						if (set == null)
+						{
+							set = new LinkedHashSet<>();
+							itemToDisplayNameMap.put(displayName, set);
+						}
+
+						set.add(itemEntry);
+					}
+				}
+			}
+
+			for (Map.Entry<String, LinkedHashSet<ItemEntry>> entry : itemToDisplayNameMap.entrySet())
+			{
+				if (entry.getValue().size() > 1)
+				{
+					list.add(entry.getKey());
+
+					for (ItemEntry itemEntry : entry.getValue())
+					{
+						builder.append('-');
+						builder.append(' ');
+						itemEntry.toString(builder);
+						list.add(builder.toString());
+						builder.setLength(0);
+					}
+
+					list.add("");
+				}
+			}
+
+			saveFile(list, "dupe_display_names.txt");
+		}).start();
+
+		new Thread(() -> {
+			List<String> list = new ArrayList<>();
+			StringBuilder builder = new StringBuilder();
+
+			for (String ore : OreDictionary.getOreNames())
+			{
+				NonNullList<ItemStack> oreItems = OreDictionary.getOres(ore);
+
+				if (oreItems.size() > 1)
+				{
+					boolean added = false;
+
+					for (ItemStack stack : oreItems)
 					{
 						ItemEntry itemEntry = ItemEntry.get(stack);
 
-						if (!stack.isEmpty() && filter(itemEntry))
+						if (!itemEntry.isEmpty() && filter(itemEntry))
 						{
-							String displayName = stack.getDisplayName();
-							LinkedHashSet<ItemEntry> set = itemToDisplayNameMap.get(displayName);
-
-							if (set == null)
+							if (!added)
 							{
-								set = new LinkedHashSet<>();
-								itemToDisplayNameMap.put(displayName, set);
+								added = true;
+								list.add(ore);
 							}
 
-							set.add(itemEntry);
-							hasRecipe.put(itemEntry, false);
-
-							ItemEntry itemEntry1 = ItemEntry.get(FurnaceRecipes.instance().getSmeltingResult(stack));
-
-							if (itemEntry.equalsEntry(itemEntry1))
-							{
-								hasRecipe.put(itemEntry, true);
-							}
-							else
-							{
-								for (IRecipe recipe : CraftingManager.REGISTRY)
-								{
-									itemEntry1 = ItemEntry.get(recipe.getRecipeOutput());
-
-									if (itemEntry.equalsEntry(itemEntry1))
-									{
-										hasRecipe.put(itemEntry, true);
-										break;
-									}
-								}
-							}
-						}
-					}
-				}
-
-				for (Map.Entry<String, LinkedHashSet<ItemEntry>> entry : itemToDisplayNameMap.entrySet())
-				{
-					if (entry.getValue().size() > 1)
-					{
-						list.add(entry.getKey());
-
-						if (md)
-						{
-							list.add("");
-						}
-
-						for (ItemEntry itemEntry : entry.getValue())
-						{
 							builder.append('-');
 							builder.append(' ');
 							itemEntry.toString(builder);
 							list.add(builder.toString());
 							builder.setLength(0);
 						}
-
-						list.add("");
 					}
+
+					list.add("");
 				}
-
-				list.add("");
-				list.add("");
-				list.add("## Ore Dictionary names with more than one item:");
-				list.add("");
-
-				for (String ore : OreDictionary.getOreNames())
-				{
-					NonNullList<ItemStack> oreItems = OreDictionary.getOres(ore);
-
-					if (oreItems.size() > 1)
-					{
-						boolean added = false;
-
-						for (ItemStack stack : oreItems)
-						{
-							ItemEntry itemEntry = ItemEntry.get(stack);
-
-							if (!itemEntry.isEmpty() && filter(itemEntry))
-							{
-								if (!added)
-								{
-									added = true;
-									list.add(ore);
-
-									if (md)
-									{
-										list.add("");
-									}
-								}
-
-								builder.append('-');
-								builder.append(' ');
-								itemEntry.toString(builder);
-								list.add(builder.toString());
-								builder.setLength(0);
-							}
-						}
-
-						list.add("");
-					}
-				}
-
-				list.add("");
-				list.add("");
-				list.add("## Items without crafting table or furnace recipe (can be inaccurate):");
-				list.add("");
-
-				for (Map.Entry<ItemEntry, Boolean> entry : hasRecipe.entrySet())
-				{
-					if (!entry.getValue())
-					{
-						builder.append('-');
-						builder.append(' ');
-						entry.getKey().toString(builder);
-						list.add(builder.toString());
-						builder.setLength(0);
-					}
-				}
-
-				list.add("");
-				list.add("");
-				list.add("## Fluids:");
-				list.add("");
-
-				for (Fluid fluid : FluidRegistry.getRegisteredFluids().values())
-				{
-					list.add("- " + fluid.getName() + " - " + new FluidStack(fluid, 1000).getLocalizedName());
-				}
-
-				FileUtils.saveSafe(new File(ClientUtils.MC.gameDir, "local/client/scanneditems." + (md ? "md" : "txt")), list);
-				ClientUtils.MC.player.sendStatusMessage(new TextComponentString("Duplicate items have been exported in ./local/client/scanneditems." + (md ? "md" : "txt") + "!"), false);
 			}
-		}.start();
+
+			saveFile(list, "ore_name_dupes.txt");
+		}).start();
+	}
+
+	private static void saveFile(List<String> list, String file)
+	{
+		File f = new File(ClientUtils.MC.gameDir, "local/client/ftbjanitor/" + file);
+		FileUtils.saveSafe(f, list);
+		ITextComponent component = new TextComponentString(file + " saved! Click here to open.");
+
+		try
+		{
+			component.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, f.getCanonicalFile().getAbsolutePath()));
+		}
+		catch (Exception ex)
+		{
+			component.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, f.getAbsolutePath()));
+		}
+
+		ClientUtils.MC.player.sendMessage(component);
 	}
 }

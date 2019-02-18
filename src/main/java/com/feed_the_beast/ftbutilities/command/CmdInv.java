973882f6c8711f073355ef34573fd52ce5de38d7
LatvianModder <latvianmodder@gmail.com>
2019-02-19 00:44:08 +0200

    Removed /inv save and /inv load

--- src/main/java/com/feed_the_beast/ftbutilities/command/CmdInv.java
+++ src/main/java/com/feed_the_beast/ftbutilities/command/CmdInv.java
@@ -1,26 +1,17 @@
 package com.feed_the_beast.ftbutilities.command;
 
-import com.feed_the_beast.ftblib.FTBLib;
-import com.feed_the_beast.ftblib.FTBLibConfig;
 import com.feed_the_beast.ftblib.lib.command.CmdBase;
 import com.feed_the_beast.ftblib.lib.command.CmdTreeBase;
 import com.feed_the_beast.ftblib.lib.command.CmdTreeHelp;
 import com.feed_the_beast.ftblib.lib.command.CommandUtils;
-import com.feed_the_beast.ftblib.lib.util.NBTUtils;
-import com.feed_the_beast.ftblib.lib.util.StringUtils;
 import com.feed_the_beast.ftbutilities.FTBUtilitiesConfig;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumHand;
 
-import javax.annotation.Nullable;
-import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
@@ -57,147 +48,6 @@ public class CmdInv extends CmdTreeBase
 		}
 	}
 
-	public static class CmdSave extends CmdBase
-	{
-		public CmdSave()
-		{
-			super("save", Level.OP);
-		}
-
-		@Override
-		public boolean isUsernameIndex(String[] args, int index)
-		{
-			return index == 0;
-		}
-
-		@Override
-		public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-		{
-			checkArgs(sender, args, 2);
-			EntityPlayerMP player = CommandUtils.getForgePlayer(sender, args[0]).getCommandPlayer(sender);
-			File file = new File(server.getDataDirectory(), "local/ftbutilities/playerinvs/" + StringUtils.fromUUID(player.getGameProfile().getId()) + "_" + args[1].toLowerCase() + ".dat");
-
-			try
-			{
-				NBTTagCompound tag = new NBTTagCompound();
-				writeItemsToNBT(player.inventory, tag, "Inventory");
-				/*IInventory baubles = InvUtils.getBaubles(ep);
-
-				if (baubles != null)
-				{
-					writeItemsToNBT(baubles, tag, "Baubles");
-				}*/
-
-				NBTUtils.writeNBT(file, tag);
-			}
-			catch (Exception ex)
-			{
-				if (FTBLibConfig.debugging.print_more_errors)
-				{
-					ex.printStackTrace();
-				}
-
-				throw FTBLib.error(sender, "error", ex.toString());
-			}
-		}
-
-		private static void writeItemsToNBT(IInventory inv, NBTTagCompound compound, String s)
-		{
-			NBTTagList nbttaglist = new NBTTagList();
-
-			for (int i = 0; i < inv.getSizeInventory(); ++i)
-			{
-				ItemStack is = inv.getStackInSlot(i);
-
-				if (!is.isEmpty())
-				{
-					NBTTagCompound nbttagcompound = is.serializeNBT();
-					nbttagcompound.setInteger("Slot", i);
-					nbttaglist.appendTag(nbttagcompound);
-				}
-			}
-
-			compound.setTag(s, nbttaglist);
-		}
-	}
-
-	public static class CmdLoad extends CmdBase
-	{
-		public CmdLoad()
-		{
-			super("load", Level.OP);
-		}
-
-		@Override
-		public boolean isUsernameIndex(String[] args, int index)
-		{
-			return index == 0;
-		}
-
-		@Override
-		public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-		{
-			checkArgs(sender, args, 2);
-			EntityPlayerMP player = CommandUtils.getForgePlayer(sender, args[0]).getCommandPlayer(sender);
-			File file = new File(server.getDataDirectory(), "local/ftbutilities/playerinvs/" + StringUtils.fromUUID(player.getGameProfile().getId()) + "_" + args[1].toLowerCase() + ".dat");
-
-			try
-			{
-				NBTTagCompound tag = NBTUtils.readNBT(file);
-				readItemsFromNBT(player.inventory, tag, "Inventory");
-				/*IInventory baubles = InvUtils.getBaubles(ep);
-
-				if (baubles != null)
-				{
-					readItemsFromNBT(baubles, tag, "Baubles");
-				}*/
-			}
-			catch (Exception ex)
-			{
-				if (FTBLibConfig.debugging.print_more_errors)
-				{
-					ex.printStackTrace();
-				}
-
-				throw FTBLib.error(sender, "error", ex.toString());
-			}
-		}
-
-		private static void readItemsFromNBT(@Nullable IInventory inv, NBTTagCompound compound, String s)
-		{
-			if (inv == null)
-			{
-				return;
-			}
-
-			NBTTagList nbttaglist = compound.getTagList(s, 10);
-
-			for (int i = 0; i < nbttaglist.tagCount(); ++i)
-			{
-				NBTTagCompound nbttagcompound = nbttaglist.getCompoundTagAt(i);
-				int j = nbttagcompound.getInteger("Slot");
-
-				if (j >= 0 && j < inv.getSizeInventory())
-				{
-					inv.setInventorySlotContents(j, new ItemStack(nbttagcompound));
-				}
-			}
-		}
-	}
-
-	public static class CmdList extends CmdBase
-	{
-		public CmdList()
-		{
-			super("list", Level.OP);
-		}
-
-		@Override
-		public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-		{
-		}
-	}
-
 	public static class CmdDisableRightClick extends CmdBase
 	{
 		public CmdDisableRightClick()
@@ -230,9 +80,6 @@ public class CmdInv extends CmdTreeBase
 	{
 		super("inv");
 		addSubcommand(new CmdView());
-		addSubcommand(new CmdSave());
-		addSubcommand(new CmdLoad());
-		//addSubcommand(new CmdList());
 		addSubcommand(new CmdDisableRightClick());
 		addSubcommand(new CmdTreeHelp(this));
 	}

43cae1fd985d6a4643d58d4dda44ba60c49acd41
insufficientchocolate <tony84727@gmail.com>
2019-04-05 20:58:42 +0800

    make TeleportType carry permission node

--- src/main/java/com/feed_the_beast/ftbutilities/data/TeleportType.java
+++ src/main/java/com/feed_the_beast/ftbutilities/data/TeleportType.java
@@ -1,12 +1,25 @@
 package com.feed_the_beast.ftbutilities.data;
 
+import com.feed_the_beast.ftblib.lib.util.misc.Node;
+import com.feed_the_beast.ftbutilities.FTBUtilitiesPermissions;
+
 public enum TeleportType
 {
-	HOME,
-	WARP,
-	BACK,
-	SPAWN,
-	TPA,
-	RTP,
-	RESPAWN,
+	HOME(FTBUtilitiesPermissions.HOMES_BACK),
+	WARP(FTBUtilitiesPermissions.WARPS_BACK),
+	BACK(null),
+	SPAWN(FTBUtilitiesPermissions.SPAWN_BACK),
+	TPA(FTBUtilitiesPermissions.TPA_BACK),
+	RTP(FTBUtilitiesPermissions.RTP_BACK),
+	RESPAWN(FTBUtilitiesPermissions.RESPAWN_BACK);
+
+	private Node permission;
+
+	TeleportType(Node node) {
+		this.permission = node;
+	}
+
+	public Node getPermissionNode() {
+		return this.permission;
+	}
 }
